9c01d3c7db9ab51a109c53d879185e8f1928087e
malicktech <malicktech@users.noreply.github.com>
2024-04-14 06:14:57 +0000

    deploy: 95523e4f6b320642de90a62460f21ecde63a547a

---
+++ assets/ContactView-Db5ty7us.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o,a as c}from"./index-CRdD1O2Z.js";const s={},a={class:"about"},n=c("h1",null,"Contact",-1),_=[n];function r(d,i){return e(),o("div",a,_)}const f=t(s,[["render",r]]);export{f as default};
