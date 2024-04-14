16c56fb2599550d9a5364e5353a30ebb848937af
Malick <malick.diop-ext@ilevia.keolis.com>
2024-04-14 07:56:41 +0200

    deploy: fix vitejs config for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// Assurez-vous de remplacer `<NOM_DU_REPO>` par le nom de votre dépôt GitHub
+const repoName = process.env.NODE_ENV === 'production' ? '/annuaire-sites-publics-senegal-ui/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: repoName,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
